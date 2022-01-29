5dc963d34eec5d69b9acf9fc89128410f3529ce1
DesktopCoder1407 <dcoder1407@gmail.com>
2022-01-29 17:51:04 -0500

    Complete learning exercise

--- Programming Assignment 01/neuralNetworkFromScratch.py
+++ Programming Assignment 01/neuralNetworkFromScratch.py
@@ -53,8 +53,60 @@ def predict(x, model):
     return A3
 
 def main():
-    import pandas as pd
-    return
+    #Scale dataset
+    from sklearn import datasets
+    boston = datasets.load_boston()
+    num_test = 10 #The last 10 samples as testing set
+    from sklearn import preprocessing
+    scaler = preprocessing.StandardScaler()
+    X_train = boston.data[:-num_test, :]
+    X_train = scaler.fit_transform(X_train)
+    y_train = boston.target[:-num_test].reshape(-1, 1)
+    X_test = boston.data[-num_test:, :]
+    X_test = scaler.transform(X_test)
+    y_test = boston.target[-num_test:]
+
+    #We can now train a one-layer neural network with 20 hidden units,
+    # a 0.1 learning rate, and 2000 iterations
+    n_hidden = 20
+    learning_rate = 0.1
+    n_iter = 2000
+    model = train(X_train, y_train, n_hidden, learning_rate, n_iter)
+
+    #Apply the trained model on the testing set
+    predictions = predict(X_test, model)
+
+    #Print out the predictions and the truths and compare them
+    print("--Predicted Values--")
+    print(predictions)
+    print("--True Values--")
+    print(y_test)
+
+    ##Implementing neural networks with scikit-learn
+    from sklearn.neural_network import MLPRegressor
+    nn_scikit = MLPRegressor(hidden_layer_sizes=(16,8),
+                            activation='relu', solver='adam', 
+                            learning_rate_init=0.001, 
+                            random_state=42, max_iter=2000)
+    nn_scikit.fit(X_train, y_train)
+    predictions = nn_scikit.predict(X_test)
+    print(predictions)
+    print(np.mean((y_test - predictions) ** 2)) #Mean Squared Error of the prediction.
+
+    ##TensorFlow neural network
+    import tensorflow as tf
+    from tensorflow import keras
+    tf.random.set_seed(42)
+    model = keras.Sequential([
+        keras.layers.Dense(units=20, activation='relu'),
+        keras.layers.Dense(units=8, activation='relu'),
+        keras.layers.Dense(units=1)
+    ])
+    model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.02))
+    model.fit(X_train, y_train, epochs=300)
+    predictions = model.predict(X_test)[:,0]
+    print(predictions)
+    print(np.mean((y_test - predictions) ** 2))
 
 if __name__ == "__main__":
     main()
