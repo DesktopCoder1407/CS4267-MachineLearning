1d3a48e10544cc3eb3acb55904eb78d91e930899
DesktopCoder1407 <dcoder1407@gmail.com>
2022-01-29 22:26:06 -0500

    Continue building Stock Prices Neural Network

---
+++ Programming Assignment 01/StockPricesNN.py
@@ -0,0 +1,117 @@
+import pandas as pd
+
+def add_original_feature(df, df_new):
+    df_new['open'] = df['Open']
+    df_new['open_1'] = df['Open'].shift(1)
+    df_new['close_1'] = df['Close'].shift(1)
+    df_new['high_1'] = df['High'].shift(1)
+    df_new['low_1'] = df['Low'].shift(1)
+    df_new['volume_1'] = df['Volume'].shift(1)
+
+def add_avg_price(df, df_new):
+    df_new['avg_price_5'] = df['Close'].rolling(5).mean().shift(1)
+    df_new['avg_price_30'] = df['Close'].rolling(21).mean().shift(1)
+    df_new['avg_price_365'] = df['Close'].rolling(252).mean().shift(1)
+
+    df_new['ratio_avg_price_5_30'] = df_new['avg_price_5'] / df_new['avg_price_30']
+    df_new['ratio_avg_price_5_365'] = df_new['avg_price_5'] / df_new['avg_price_365']
+    df_new['ratio_avg_price_30_365'] = df_new['avg_price_30'] / df_new['avg_price_365']
+
+def add_avg_volume(df, df_new):
+    df_new['avg_volume_5'] = df['Close'].rolling(5).mean().shift(1)
+    df_new['avg_volume_30'] = df['Close'].rolling(21).mean().shift(1)
+    df_new['avg_volume_365'] = df['Close'].rolling(252).mean().shift(1)
+
+    df_new['ratio_avg_volume_5_30'] = df_new['avg_volume_5'] / df_new['avg_volume_30']
+    df_new['ratio_avg_volume_5_365'] = df_new['avg_volume_5'] / df_new['avg_volume_365']
+    df_new['ratio_avg_volume_30_365'] = df_new['avg_volume_30'] / df_new['avg_volume_365']
+
+def add_std_price(df, df_new):
+    df_new['std_price_5'] = df['Close'].rolling(5).std().shift(1)
+    df_new['std_price_30'] = df['Close'].rolling(21).std().shift(1)
+    df_new['std_price_365'] = df['Close'].rolling(252).std().shift(1)
+
+    df_new['ratio_std_price_5_30'] = df_new['std_price_5'] / df_new['std_price_30']
+    df_new['ratio_std_price_5_365'] = df_new['std_price_5'] / df_new['std_price_365']
+    df_new['ratio_std_price_30_365'] = df_new['std_price_30'] / df_new['std_price_365']
+
+def add_std_volume(df, df_new):
+    df_new['std_volume_5'] = df['Close'].rolling(5).std().shift(1)
+    df_new['std_volume_30'] = df['Close'].rolling(21).std().shift(1)
+    df_new['std_volume_365'] = df['Close'].rolling(252).std().shift(1)
+
+    df_new['ratio_std_volume_5_30'] = df_new['std_volume_5'] / df_new['std_volume_30']
+    df_new['ratio_std_volume_5_365'] = df_new['std_volume_5'] / df_new['std_volume_365']
+    df_new['ratio_std_volume_30_365'] = df_new['std_volume_30'] / df_new['std_volume_365']
+
+def add_return_feature(df, df_new):
+    df_new['return_1'] = ((df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)).shift(1)
+    df_new['return_5'] = ((df['Close'] - df['Close'].shift(5)) / df['Close'].shift(5)).shift(1)
+    df_new['return_30'] = ((df['Close'] - df['Close'].shift(21)) / df['Close'].shift(21)).shift(1)
+    df_new['return_365'] = ((df['Close'] - df['Close'].shift(252)) / df['Close'].shift(252)).shift(1)
+    df_new['moving_avg_5'] = df_new['return_1'].rolling(5).mean().shift(1)
+    df_new['moving_avg_30'] = df_new['return_1'].rolling(21).mean().shift(1)
+    df_new['moving_avg_365'] = df_new['return_1'].rolling(252).mean().shift(1)
+
+def generate_features(df):
+    """
+    Generate features for a stock/index based on historical price and performance
+    @param df: dataframe with columns "Open", "Close", "High", "Low", "Volume", "Adjusted Close"
+    @return: dataframe, dataset with new features
+    """
+    df_new = pd.DataFrame()
+    # 6 original features
+    add_original_feature(df, df_new)
+    # 31 generated features
+    add_avg_price(df, df_new)
+    add_avg_volume(df, df_new)
+    add_std_price(df, df_new)
+    add_std_volume(df, df_new)
+    add_return_feature(df, df_new)
+    # the target
+    df_new['close'] = df['Close']
+    df_new = df_new.dropna(axis=0)
+    return df_new
+
+data_raw = pd.read_csv('19880101_20191231.csv', index_col='Date')
+data = generate_features(data_raw)
+
+#Training and testing points
+start_train = '1988-01-01'
+end_train = '2018-12-31'
+start_test = '2019-01-01'
+end_test = '2019-12-31'
+
+#Training set
+data_train = data.loc[start_train:end_train]
+X_train = data_train.drop('close', axis=1).values
+y_train = data_train['close'].values
+
+#Testing set
+data_test = data.loc[start_test:end_test]
+X_test = data_test.drop('close', axis=1).values
+y_test = data_test['close'].values
+
+#Normalizing
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_scaled_train = scaler.fit_transform(X_train)
+X_scaled_test = scaler.transform(X_test)
+
+#Build Neural Network Model
+import tensorflow as tf
+from keras import Sequential
+from keras.layers import Dense
+model = Sequential([
+    Dense(units=32, activation='relu'),
+    Dense(units=1)
+])
+model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
+model.fit(X_scaled_train, y_train, epochs=100, verbose=True)
+predictions = model.predict(X_scaled_test)
+
+#Predict the test data and display metrics
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+print("MSE: %.3f" % mean_squared_error(y_test, predictions))
+print("MAE: %.3f" % mean_absolute_error(y_test, predictions))
+print("R^2: %.3f" % r2_score(y_test, predictions))
