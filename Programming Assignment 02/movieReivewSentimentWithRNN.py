8039b7f5dbf21135c720e86a312fb601c8d13430
DesktopCoder1407 <dcoder1407@gmail.com>
2022-02-03 07:56:54 -0500

    Complete Programming Assignment 2

--- Programming Assignment 02/movieReivewSentimentWithRNN.py
+++ Programming Assignment 02/movieReivewSentimentWithRNN.py
@@ -28,8 +28,8 @@ def analyzeData():
     import matplotlib.pyplot as plt
     plt.hist(review_lengths, bins=10)
     plt.show()
+analyzeData()
 
-#analyzeData()
 # --PREPROCESS DATA--
 #Pad or truncate all data so it is the length of maxlen
 maxlen = 200 #The length of the majority of the reviews
@@ -38,3 +38,64 @@ X_test = pad_sequences(X_test, maxlen=maxlen)
 
 print('X_train shape after padding:', X_train.shape)
 print('X_test shape after padding:', X_test.shape)
+
+# --DEVLOP A SIMPLE LSTM (Long Short-Term Memory) NETWORK--
+def createSimpleLSTM():
+    tf.random.set_seed(42)
+    model = models.Sequential()
+
+    #Embed our input (with a size of vocab_size) into dense vectors of size embedding_size
+    embedding_size = 32
+    model.add(layers.Embedding(vocab_size, embedding_size))
+
+    #Recurrent, LSTM layer with 50 nodes
+    model.add(layers.LSTM(50))
+
+    #Output layer with sigmoid activation function (sigmoid chosen because the problem is binary classification)
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    #Model summary. Shows all layers.
+    print(model.summary())
+
+    #Compile model with Adam
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    #Train & validate the model. Batch size of batch_size, Epochs of n_epoch
+    batch_size = 64
+    n_epoch = 3
+    model.fit(X_train, y_train, batch_size=batch_size, epochs=n_epoch, validation_data=(X_test, y_test))
+
+    #Evaluate the classification accuracy
+    acc = model.evaluate(X_test, y_test, verbose=0)[1]
+    print('Test accuracy:', acc)
+createSimpleLSTM()
+
+# --PERFORMANCE BOOST WITH MULTIPLE LSTM LAYERS--
+def createMultipleLSTM():
+    #Embed our input (with a size of vocab_size) into dense vectors of size embedding_size
+    tf.random.set_seed(42)
+    embedding_size = 32
+
+    model = models.Sequential()
+    model.add(layers.Embedding(vocab_size, embedding_size)) #Same as first model
+    model.add(layers.LSTM(50, return_sequences=True, dropout=0.2)) #50 nodes, return_sequences=True 
+        #to feed the entire output sequence to the second LSTM, dropout of 20% to reduce overfitting.
+    model.add(layers.LSTM(50, dropout=0.2)) #50 nodes, dropout of 20% to reduce overfitting.
+    model.add(layers.Dense(1, activation='sigmoid')) #Same as first model
+
+    #Model summary. Shows all layers.
+    print(model.summary())
+
+    #Compile model with Adam
+    optimizer = tf.keras.optimizers.Adam(lr=0.003)
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    #Train & validate the model. Batch size of batch_size, Epochs of n_epoch
+    batch_size = 64
+    n_epoch = 7
+    model.fit(X_train, y_train, batch_size=batch_size, epochs=n_epoch, validation_data=(X_test, y_test))
+
+    #Evaluate the test accuracy
+    acc = model.evaluate(X_test, y_test, verbose=0)[1]
+    print('Test accuracy with stacked LSTM:', acc)
+createMultipleLSTM()
